e340930b5498f112c358eafddff221867c17401a
riri0427 <riri190427@gmail.com>
2019-05-29 21:59:04 +0900

    Create articles table

--- db/migrate/20190529123604_create_articles.rb
+++ db/migrate/20190529123604_create_articles.rb
@@ -1,8 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
-      t.timestamps
+      t.string     :title,   null: false
+      t.text       :text,    null: false
+      t.references :user,    null: false, foreign_key: true
+      t.timestamps           null: false
     end
   end
 end
